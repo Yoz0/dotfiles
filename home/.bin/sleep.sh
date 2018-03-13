267f76e03db9ddaaef79435a503e66d0b4476192
Kastenbaum Stephane <stephane.kastenbaum@gmail.com>
2018-03-13 20:11:26 +0100

    Add fading to screenlock and sleep
    This is prettier, and now sleep actually works as intended !

--- home/.bin/sleep.sh
+++ home/.bin/sleep.sh
@@ -5,8 +5,9 @@ dbus-send --print-reply --dest=com.github.chjj.compton.${DISPLAY/:/_} / \
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-i3lock -n -i /tmp/screen.png
-# Disable compton fading
+i3lock -i /tmp/screen.png
+systemctl suspend
+#Disable compton fading
 dbus-send --print-reply --dest=com.github.chjj.compton.${DISPLAY/:/_} / \
       com.github.chjj.compton.opts_set string:no_fading_openclose boolean:true
 rm /tmp/screen.png
